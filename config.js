e6e996053d9bc4392d6c8b4886a600d2b6864d16
nn <205666415+guessmesome@users.noreply.github.com>
2025-11-09 15:39:08 +0700

    upd

---
+++ config.js
@@ -0,0 +1,7 @@
+const productConfig = {
+    product1: 'https://trnsfx.ru/go/s9mqh0f7ge',
+    product2: 'https://trnsfx.ru/go/sv4b60czzs',
+    product3: 'https://trnsfx.ru/go/swdzbe35hg',
+    product4: 'https://trnsfx.ru/go/spm1xyczj5'
+};
+
